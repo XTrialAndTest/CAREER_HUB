a055a013659cc1cc2c39e81278212e2ab24b544d
Xtrialandtest <1trialandtest@gmail.com>
2023-05-25 10:11:44 +0300

    render connected

--- CareerHub_Project/settings.py
+++ CareerHub_Project/settings.py
@@ -88,21 +88,21 @@ WSGI_APPLICATION = 'CareerHub_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Life DataBase from Render 
 # Render Configuration
 
-# DATABASES = {
-#     'default': dj_database_url.parse(config('POST_KEY'))
+DATABASES = {
+    'default': dj_database_url.parse(config('POST_KEY'))
 
 
-# }
+}
 
 
 
