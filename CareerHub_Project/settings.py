e807ff87a6b31a2a1883b14d3d0e95e747157500
Xtrialandtest <1trialandtest@gmail.com>
2023-05-25 11:49:21 +0300

    render

--- CareerHub_Project/settings.py
+++ CareerHub_Project/settings.py
@@ -29,12 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = config("SECRET_KEY")
-
+SECRET_KEY = 'django-insecure-ms@^7a61!9*u8%x!=8%n*k1rqhmf5kv%x+uh$g57+7z#q-_nvz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
